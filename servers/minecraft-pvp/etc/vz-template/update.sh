f77f68e0878332144eccd9acf4c024397b4b24a3
Justin Duplessis <drfoliberg@gmail.com>
2015-02-23 12:26:24 -0500

    Add minecraft server

---
+++ servers/minecraft-pvp/etc/vz-template/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+VER=`curl -s https://s3.amazonaws.com/Minecraft.Download/versions/versions.json | jsawk -n 'out(this.latest.release)'`
+wget https://s3.amazonaws.com/Minecraft.Download/versions/$VER/minecraft_server.$VER.jar -O /home/minecraft/server/minecraft_server.jar
+chown minecraft /home/minecraft/server/minecraft_server.jar
