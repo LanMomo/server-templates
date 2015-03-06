0ce68d6b284355a8fd1043df93149fd9d06966a4
Justin Duplessis <drfoliberg@gmail.com>
2015-03-06 00:13:54 -0500

    Dynamic year in server name
    Remove script from init before inserting.
    
    Protect here-doc var scope.

--- servers/global/etc/vz-template/scripts/auto_hostname.sh
+++ servers/global/etc/vz-template/scripts/auto_hostname.sh
@@ -2,14 +2,14 @@
 # Script to inject a one time boot script.
 # Should be run each time inside in the container before converting to template.
 
-cat > /etc/init.d/auto_hostname << EOF
+cat > /etc/init.d/auto_hostname << 'EOF'
 #!/bin/bash
 # /etc/init.d/auto_hostname
 # Script to run on the first boot of the template.
 # Calls set_server_name if executable and removes itself.
 
 if [[ -x /etc/vz-template/set_server_name.sh ]]; then
-    base_name="LAN Montmorency 2015"
+    base_name="LAN Montmorency $(date +%Y)"
     hostname=$(hostname | cut -d '.' -f 1)
 
     /etc/vz-template/set_server_name.sh "${base_name} - ${hostname}"
@@ -22,5 +22,6 @@ rm -f /etc/init.d/auto_hostname
 
 EOF
 
+update-rc.d -f auto_hostname remove
 chmod +x /etc/init.d/auto_hostname
 update-rc.d auto_hostname defaults
