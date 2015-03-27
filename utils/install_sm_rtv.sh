dfa55219228ed9ae8b851d62268ea66e242be8fc
Justin Duplessis <drfoliberg@gmail.com>
2015-03-26 22:46:51 -0400

    Add util script to install RTV.

---
+++ utils/install_sm_rtv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+# Installs metamod and sourcemod and enables rtv plugin.
+
+wget http://mirror.pointysoftware.net/alliedmodders/mmsource-1.10.4-linux.tar.gz -O mmsource.tar.gz
+tar -zxf mmsource.tar.gz
+rm mmsource.tar.gz
+
+wget http://mirror.pointysoftware.net/alliedmodders/sourcemod-1.7.0-linux.tar.gz -O sourcemod.tar.gz
+tar -zxf sourcemod.tar.gz
+rm sourcemod.tar.gz
+
+mv addons/sourcemod/plugins/disabled/{mapchooser.smx,rockthevote.smx} addons/sourcemod/plugins/
