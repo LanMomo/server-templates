28a47409b516c33525bc3c4355fdc63984e88222
Justin Duplessis <jduplessis@iweb.com>
2015-04-20 16:20:02 -0400

    partial config fixes for lan

--- servers/valve/etc/vz-template/set_admin_pass.sh
+++ servers/valve/etc/vz-template/set_admin_pass.sh
@@ -4,4 +4,4 @@ password=$1
 
 . /etc/vz-template/template_vars.sh
 
-sed -i "s/^rcon_password.*/rcon_password ${password}/" "$base_config_file"
+sed -i "s/^rcon_password.*/rcon_password \"${password}\"/" "$base_config_file"
