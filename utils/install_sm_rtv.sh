4216b3a5146d792c4022b66c44db9e6bdef389c2
Justin Duplessis <drfoliberg@gmail.com>
2015-11-08 13:35:30 -0500

    Update sm and mm versions

--- utils/install_sm_rtv.sh
+++ utils/install_sm_rtv.sh
@@ -3,11 +3,11 @@
 
 # TODO move this to sourcemod and get gameserver path from template
 
-wget http://mirror.pointysoftware.net/alliedmodders/mmsource-1.10.4-linux.tar.gz -O mmsource.tar.gz
+wget http://cdn.probablyaserver.com/sourcemod/mmsource-1.10.6-linux.tar.gz -O mmsource.tar.gz
 tar -zxf mmsource.tar.gz
 rm mmsource.tar.gz
 
-wget http://mirror.pointysoftware.net/alliedmodders/sourcemod-1.7.0-linux.tar.gz -O sourcemod.tar.gz
+wget http://www.sourcemod.net/smdrop/1.7/sourcemod-1.7.3-git5273-linux.tar.gz -O sourcemod.tar.gz
 tar -zxf sourcemod.tar.gz
 rm sourcemod.tar.gz
 
